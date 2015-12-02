fa184eacd55951aae0b183126981cfc78220b234
Chris Stadler <chrisstadler@gmail.com>
2015-12-02 11:12:00 -0500

    switch to controlled input

--- app/src/components/get_athlete_form.jsx
+++ app/src/components/get_athlete_form.jsx
@@ -1,17 +1,28 @@
 var React = require('react');
 
 var GetAthleteForm = React.createClass({
+    getInitialState: function() {
+        return {tfrrs_id: ''};
+    },
+
+    update_id: function(event) {
+        this.setState({tfrrs_id: event.target.value});
+    },
+
     get_athlete: function(e) {
 		e.preventDefault();
-		var url = this.refs.tfrrs_url.getDOMNode().value;
-		console.log(url);
-        this.props.add_athlete(url);
+		var id = this.state.tfrrs_id;
+		console.log(id);
+        this.props.add_athlete(id);
 	},
+
     render: function() {
         return (
             <form id='get-url' action="/" method="GET" onSubmit={this.get_athlete}>
-                <label htmlFor="tfrrs_url">tffrs.org/athletes/</label>
-                <input type="text" name="tfrrs_url" id="tfrrs_url" ref="tfrrs_url"/>
+                <label htmlFor="tfrrs_id">tffrs.org/athletes/</label>
+                <input type="text" name="tfrrs_id" id="tfrrs_id"
+                    value={this.state.tfrrs_id}
+                    onChange={this.update_id}/>
                 <input type="submit" value="Get Data" />
             </form>
         )
